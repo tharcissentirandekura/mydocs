ce54322c89ea4c1bc20de1c9be6489877f6b01e1
tharcissentirandekura <tntirand@oberlin.edu>
2024-01-12 12:29:05 -0500

    save

--- board_colors.py
+++ board_colors.py
@@ -17,10 +17,12 @@ class MUltiSquare(SampleBase):
         
         
         while True:
+           
             for x in range(-1,self.matrix.width):
                 #canvas =  self.matrix.SwapOnVSync(canvas) running one color at a time and change for each iteration
                 for y in range(-1,self.matrix.height):
                     canvas.SetPixel((self.matrix.width //20) * x,(self.matrix.height // 20) * y,self.red,self.green,self.blue)
+                canvas.Fill(20,70,70)
                 canvas =  self.matrix.SwapOnVSync(canvas)
             time.sleep(2)
             print((self.red,self.green,self.blue))
