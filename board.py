54298a3fa5b025e2019ca0ce203efe3c7120fa93
tharcissentirandekura <tntirand@oberlin.edu>
2024-01-12 11:52:30 -0500

    updating the board file

--- board.py
+++ board.py
@@ -1,5 +1,6 @@
 from samplebase import SampleBase
 import sys
+import time
 
 class MUltiSquare(SampleBase):
 
@@ -10,16 +11,23 @@ class MUltiSquare(SampleBase):
 
         self.red = 255
         self.green = 10
-        self.blue = 200
+        self.blue = 20
         canvas = self.matrix.CreateFrameCanvas()  
+        
+        
+        
         while True:
             for x in range(-1,self.matrix.width):
+                #canvas =  self.matrix.SwapOnVSync(canvas) running one color at a time and change for each iteration
                 for y in range(-1,self.matrix.height):
-                    canvas.SetPixel((self.matrix.width //8) * x,(self.matrix.height // 8) * y,self.red,self.green,self.blue)
-
-                self.green = (self.green + 20) % 255
-                self.red = (self.red - 20) % 250
+                    canvas.SetPixel((self.matrix.width //20) * x,(self.matrix.height // 20) * y,self.red,self.green,self.blue)
                 canvas =  self.matrix.SwapOnVSync(canvas)
+            time.sleep(2)
+            print((self.red,self.green,self.blue))
+            self.green = (self.green + 15 * x) % 255
+            self.red = (self.red - 15 *y) % 255
+            self.blue = (self.blue + 20 *y) % 255
+            
 
 
 if __name__ == "__main__":
