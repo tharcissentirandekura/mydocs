180b76f201867e2b6a9dc72e4cffcd0f270ce92f
tharcissentirandekura <tntirand@oberlin.edu>
2024-01-12 10:54:12 -0500

    save

--- board.py
+++ board.py
@@ -6,16 +6,15 @@ class MUltiSquare(SampleBase):
     def __init__(self, *args, **kwargs):
         super(MUltiSquare, self).__init__(*args, **kwargs)
 
-    def run(self,width,height):
-        self.width = width
-        self.height = height
+    def run(self):
+
         self.red = 255
         self.green = 10
         self.blue = 200
-        canvas = self.matrix.CreateFrameCanvas()
+        canvas = self.matrix.CreateFrameCanvas()  
         for x in range(-1,int(sys.argv[1])*2):
             for y in range(int(sys.argv[1])*2):
-                canvas.SetPixel((self.width //int(sys.argv[1])) * x,(self.height // int(sys.argv[1])) * y,self.red,self.green,self.blue)
+                canvas.SetPixel((self.matrix.width //int(sys.argv[1])) * x,(self.matrix.height // int(sys.argv[1])) * y,self.red,self.green,self.blue)
 
             self.green = (self.green + 20) % 255
             self.red = (self.red - 20) % 250
@@ -23,6 +22,7 @@ class MUltiSquare(SampleBase):
 
 
 if __name__ == "__main__":
+    
     grid = MUltiSquare()
     if (not grid.process()):
         grid.print_help()
