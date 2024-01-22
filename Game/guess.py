09852c4d38fb30e9ffd93b469f60fda2661b073f
tharcissentirandekura <tntirand@oberlin.edu>
2024-01-21 22:38:54 -0500

    save the changes

---
+++ Game/guess.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append('/Users/tntirand/Desktop/mydocs/scroll') 
+from scroll import picture
+picture.new_picture(400,400)
+
+# picture.set_fill_color("black")
+# picture.draw_filled_rectangle(200,200,40,20)
+# picture.display()
+
+
